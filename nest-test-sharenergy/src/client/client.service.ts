5db8bedcf79bfff03d1ec7bcdc0872880304b6bf
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-11 14:58:40 -0300

    feat(client.ser): add prisma constructor

--- nest-test-sharenergy/src/client/client.service.ts
+++ nest-test-sharenergy/src/client/client.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 
 @Injectable()
 export class ClientService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createClientDto: CreateClientDto) {
     return 'This action adds a new client';
   }
@@ -12,15 +15,15 @@ export class ClientService {
     return `This action returns all client`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} client`;
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
+  update(id: string, updateClientDto: UpdateClientDto) {
     return `This action updates a #${id} client`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} client`;
   }
 }
